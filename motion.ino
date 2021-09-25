1228ef8ef060141837806e21739fcb48b25a3276
Daniel J. Greenhoe <dgreenhoe@pmsi.cc>
2021-09-25 19:05:24 -0400

    analog read

--- motion.ino
+++ motion.ino
@@ -11,11 +11,13 @@
 //   (1) Arduino Uno R3 board
 //   (2) HC-SR501 PIR Motion Sensor module
 // References
-//   (1) HC-SR501 (uses BISS0001 IC and 
+//   (1) HC-SR501
+//       https://www.amazon.com/dp/B07KZW86YR 
 //       https://learn.adafruit.com/pir-passive-infrared-proximity-motion-sensor
 //       https://diyi0t.com/hc-sr501-pir-motion-sensor-arduino-esp8266-esp32/
 //       https://www.epitran.it/ebayDrive/datasheet/44.pdf
 //       https://www.mpja.com/download/31227sc.pdf
+//       https://youtu.be/M4q85neFwjE  Andreas Spiess "#325 6 PIR Sensors tested..."
 //        _____________________
 //       |                    o|<--jumper top two for single detect
 //       |  BOTTOM VIEW       o|
@@ -47,11 +49,17 @@ void setup()
 //-----------------------------------------------------------------------------
 void loop() {
   char buf[128];
-  int sensorState = digitalRead(sensorPIR);
+  char buf2[128];
+//int sensorState = digitalRead(sensorPIR);
+  int sensorState = analogRead(sensorPIR);
+  float outVal = (float)sensorState/4095.0;
+  delay(5);
   digitalWrite(LED_BUILTIN, HIGH);   // switch LED on
-  //sprintf(buf, "sensorState=%d\n", sensorState);
-  //Serial.write(buf);
-  Serial.println(5*sensorState);  // use Serial.println for use with "Serial Plotter"
+  dtostrf(outVal,6,2,buf2);
+  sprintf(buf, "sensorState=%d  %s\n", sensorState, buf2);
+  Serial.write(buf);
+  //Serial.println(5*sensorState);  // use Serial.println for use with "Serial Plotter"
+  //Serial.println(outVal);  // use Serial.println for use with "Serial Plotter"
   if(sensorState==HIGH)
   {  
     delay(5);
